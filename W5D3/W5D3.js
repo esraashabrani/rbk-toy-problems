65d272d731a921f8fe6d6346de27e264aada94b5
esraashabrani <esraashabrani@gmail.com>
2020-05-04 09:43:12 +0300

    W5D3

--- W5D3/W5D3.js
+++ W5D3/W5D3.js
@@ -37,3 +37,59 @@ orderAVegetarianDish(staffA); //true
 */
 
 // your answer is here 
+function orderAVegetarianDish(array){
+  return reduce(array, function(result, element) {
+    return result || element['mealPreferences']=== 'vegetarian';
+  }, false);
+ 
+}
+
+
+function each(coll, func) {
+  if(Array.isArray(coll)){
+    for(var i=0 ; i<coll.length ; i++){
+      func(coll[i],i)
+    }
+  }
+  else{
+    for(var key in coll){
+      func(coll[key],key)
+    }
+  }
+}
+
+function reduce(array, f, start) {
+  var acc = start;
+  each(array, function(element) {
+    acc = f(acc, element);
+  });
+  return acc;
+}
+
+
+var staffA = [
+  {
+    name: "Lia",
+    department: "Human Resources",
+    dietaryRestrictions: [],
+    mealPreferences: "no preferences"
+  },
+  {
+    name: "Sebastian",
+    department: "Engineering",
+    dietaryRestrictions: ["dairy free", "gluten free"],
+    mealPreferences: "vegetarian"
+  },
+  {
+    name: "Ari",
+    department: "Executive",
+    dietaryRestrictions: ["dairy free"],
+    mealPreferences: "vegetarian"
+  },
+  {
+    name: "Martha",
+    department: "Legal",
+    dietaryRestrictions: ["nut allergies"],
+    mealPreferences: "non-vegetarian"
+  }
+];
